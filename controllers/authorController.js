a8f44507ce6767445ae58fc770a54ab7ee49788a
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-11-07 10:45:47 -0800

    Initial Commit

---
+++ controllers/authorController.js
@@ -0,0 +1,66 @@
+const Author = require('../models/author');
+const Book = require('../models/book');
+const asyncHandler = require('express-async-handler');
+
+// Display list of all Authors.
+exports.author_list = asyncHandler(async (req, res, next) => {
+    const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
+    res.render("index", {
+        title: "Author List",
+        author_list: allAuthors,
+        route: 'author_list.ejs',
+    });
+});
+
+// Display detail page for a specific Author.
+exports.author_detail = asyncHandler(async (req, res, next) => {
+  // Get details of author and all their books (in parallel)
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (author === null) {
+    // No results.
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("index", {
+    title: "Author Detail",
+    author: author,
+    author_books: allBooksByAuthor,
+    route: "author_detail.ejs",
+  });
+});
+
+// Display Author create form on GET.
+exports.author_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Author create GET");
+});
+
+// Handle Author create on POST.
+exports.author_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Author create POST");
+});
+
+// Display Author delete form on GET.
+exports.author_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Author delete GET");
+});
+
+// Handle Author delete on POST.
+exports.author_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Author delete POST");
+});
+
+// Display Author update form on GET.
+exports.author_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Author update GET");
+});
+
+// Handle Author update on POST.
+exports.author_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Author update POST");
+});
---
+++ controllers/bookInstanceController.js
@@ -0,0 +1,64 @@
+const BookInstance = require("../models/bookinstance");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all BookInstances.
+exports.bookinstance_list = asyncHandler(async (req, res, next) => {
+    const allBookInstances = 
+        await BookInstance.find().populate("book").exec();
+    
+    res.render("index", { 
+        title: "Book Instance List", 
+        bookinstance_list: allBookInstances, 
+        route: 'bookinstance_list.ejs'
+    });
+});
+
+// Display detail page for a specific BookInstance.
+exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
+    const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    // No results.
+    const err = new Error("Book copy not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("index", {
+    title: "Book:",
+    bookinstance: bookInstance,
+    route: "bookinstance_detail.ejs",
+  });
+});
+
+// Display BookInstance create form on GET.
+exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: BookInstance create GET");
+});
+
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: BookInstance create POST");
+});
+
+// Display BookInstance delete form on GET.
+exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: BookInstance delete GET");
+});
+
+// Handle BookInstance delete on POST.
+exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: BookInstance delete POST");
+});
+
+// Display BookInstance update form on GET.
+exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: BookInstance update GET");
+});
+
+// Handle BookInstance update on POST.
+exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
+    res.send("NOT IMPLEMENTED: BookInstance update POST");
+});
