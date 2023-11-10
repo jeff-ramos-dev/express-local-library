6490df3ed653666ed1d967abe8c682dd3fe67e04
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-11-09 18:36:56 -0800

    Add compression

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog"); // Import routes for 'catalog' area of site
+const compression = require("compression");
 
 const app = express();
 const mongoose = require("mongoose");
@@ -28,6 +29,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
